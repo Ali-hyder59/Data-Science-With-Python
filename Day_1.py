bd39289f2f917b730961af3446144924ca3cd30c
Ali-hyder59 <125720821+Ali-hyder59@users.noreply.github.com>
2023-03-19 18:07:27 +0500

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,17 @@
+#print(int(8/3))
+#print(round(8/3))
+#print(type(round(8/3,2)))
+#result = 4/2
+#result/=2
+#print(result)
+#score=23
+#height= 4.6
+#wimmimg=False
+#print(f"Your score is {score}, your height is {height},you are winning is {wimmimg} ")
+#age = input("What is your age?")
+#age_as_int= int(age)
+#years_remaining= 90-age_as_int
+#days_remaining= years_remaining * 365
+#weeks_remaining= years_remaining * 52
+#months_remaining= years_remaining * 12
+#print(f"You have {days_remaining}days,{weeks_remaining}weeks,{months_remaining}months are left  ")
